faadec71ba3e87aaf53594b4c985580dcc30acf6
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-02-01 10:28:59 -0600

    fix repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, nmb) {
+    let repeatedString = '';
+
+    if (nmb < 0) {
+        return 'ERROR'
+    } else if (nmb > 0) {
+        while (nmb > 0) {
+        repeatedString += str;
+        nmb--;
+        }
+        return repeatedString;
+    } else {
+        return ''
+    }
 
 };
 
