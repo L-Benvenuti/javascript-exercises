7d41d7501de0090b19063b88ef9cbc1cc98aebef
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-02-01 15:11:45 -0600

    fix tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+    let cResult = (far - 32) * 5/9;
+    let cRounded = Math.round(cResult * 10)/10;
+    return cRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+    fResult = (cel * 9/5 + 32);
+    let fRounded = Math.round(fResult * 10)/10;
+    return fRounded;
 };
 
 // Do not edit below this line
