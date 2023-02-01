d1166d50fea578889ae0680615c7593ef4dd90c0
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-02-01 14:52:27 -0600

    fix sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    if (typeof a === 'string' || typeof b === 'string') {
+        return 'ERROR'
+    } else if (a > b) {
+        for (i = b; i <= a; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else if (a > 0 && b > 0) {
+    for (i = a; i <= b; i++) {
+        finalSum += i;
+    }
+    return finalSum;
+    } else {
+        return 'ERROR'
+    }    
 };
 
 // Do not edit below this line
