b1e92125ae2735a2f445fe7ce25094b86e8edc78
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-02-01 13:39:38 -0600

    fix removeFromArrays.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...items) {
+    const newArray = [];
+    arr.forEach((item) => {
+        if(!items.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
