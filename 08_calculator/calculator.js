8c3b4e8980ee5032bfdf813077a1ccd6e5017f11
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-03-22 10:56:12 -0500

    change: 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(item => {sum += item});
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = arr.reduce((a, b) => a*b);
+  return mult
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let power = 1;
+  for (let i = 0; i < b; i++){
+    power *= a
+  };
+  return power
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 0) {
+    return -1;
+  } else if (a == 0) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 };
 
 // Do not edit below this line
