ae00b191aa9c50ebb9fe34b7794930d899d2b76f
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-03-22 11:15:29 -0500

    change: 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "") // replace/delete punctiation (replaces for an empty string)
+            .replace(/\s+/g, "") // replace/delete whitespaces (replaces for an empty string
+            .toLowerCase();
+    let reverse = str.split('').reverse().join('');
+    return str === reverse
 };
 
 // Do not edit below this line
